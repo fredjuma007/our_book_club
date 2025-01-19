01455abf79a56fd6c064f5566a7c72c0d5b5a6eb
fred <fredjuma8@gmail.com>
2025-01-19 22:16:50 +0300

    updated landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
           height={200}
           className="mx-auto mb-8 rounded-full border-4 border-gray-300 shadow-lg transform transition-all duration-500 ease-in-out hover:scale-110 hover:rotate-6 animate__animated animate__fadeIn animate__delay-1s"
         />
-        <h1 className="text-4xl font-bold mb-4">WELCOME TO READING CIRCLES</h1>
+        <h1 className="text-4xl font-bold mb-4">WELCOME TO READING CIRCLE</h1>
         <p className="text-xl text-gray-600 mb-8">
           Discover, share, and review your favorite books with a community of
           book lovers.
