f53c4be49ecb8f62a7e2d293a6e6142a7ea4ab6f
fred <fredjuma8@gmail.com>
2025-01-22 01:20:55 +0300

    Edited the landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             className="mx-auto mb-8 rounded-full border-4 border-gray-300 shadow-lg transform transition-all duration-500 ease-in-out hover:scale-110 hover:rotate-6 animate__animated animate__fadeIn animate__delay-1s"
           />
             <h1 className="text-4xl font-bold mb-4">
-            WELCOME TO <span className="text-green-500">READING CIRCLE</span>
+            WELCOME TO <span className="text-green-500">THE READING CIRCLE</span>
             </h1>
             <p className="text-xl text-gray-600 mb-8">
             Discover, share, and review your <span className="text-green-500"> favorite books</span> with a community of
