8d423246e8a706b08a164e2aa4e9dbf8a997dd4e
fred <fredjuma8@gmail.com>
2025-02-24 18:35:33 +0300

    added a backround image and updated event page

--- src/app/club-events/page.tsx
+++ src/app/club-events/page.tsx
@@ -14,18 +14,9 @@ export default function EventsPage() {
   const events = [
     {
       id: 1,
-      title: "February Outing Romance Book Discussion ❤️ & Bottle Painting 🎨",
-      date: new Date(2025, 1, 22),
-      moderators: ["John Njuki", "Elizabeth", "Emily Wanjah : Bottle Painting 🎨"],
-      description: "A special book discussion at The Nairobi Arboretum. The book of the month is Vicious (Sinners of Saint Book 1) by L.J. Shen 📚. We will also have a bottle painting session 🎨. What to Carry:♦️Glass bottles (like wine bottles) 🍷♦️Snacks 🍎",
-      imageUrl: "/feb poster.jpeg",
-      link: "https://www.google.com/maps/dir//Kilimani+Arboretum+Rd,+off+State+House+Rd,+Nairobi",
-    },
-    {
-      id: 2,
       title: "Book Club Meeting",
       date: new Date(2025, 2, 29), // highlighted date
-      moderators: ["BookClub Member", "BookClub Member"],
+      moderators: ["BookClub Member", "Immanuel Njogu"],
       description:
         "Join us for our monthly book club meeting to discuss the latest read!.",
       imageUrl: "/logo.jpeg",
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,8 +4,11 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <div className="pt-24 flex justify-center flex-grow p-8 font-[family-name:var(--font-geist-sans)]">
+    <div className="relative flex flex-col min-h-screen bg-[url('/picnic.jpg')] bg-cover bg-center bg-no-repeat">
+      {/* Dark Overlay */}
+      <div className="absolute inset-0 bg-black bg-opacity-80"></div>
+
+      <div className="relative pt-24 flex justify-center flex-grow p-8 font-[family-name:var(--font-geist-sans)]">
         <div className="text-center max-w-3xl">
           <Image
             src="/logo.jpeg"
@@ -14,14 +17,15 @@ export default function Home() {
             height={200}
             className="mx-auto mb-8 rounded-full border-4 border-gray-300 shadow-lg transform transition-all duration-500 ease-in-out hover:scale-110 hover:rotate-6 animate__animated animate__fadeIn animate__delay-1s"
           />
-            <h1 className="text-4xl font-bold mb-4">
+          <h1 className="text-4xl font-bold mb-4 text-white">
             WELCOME TO <span className="text-green-500">THE READING CIRCLE</span>
-            </h1>
-            <p className="text-xl text-gray-600 mb-8">
-            Discover, share, and review your <span className="text-green-500"> favorite books</span> with a community of
-            <span className="text-green-500"> book lovers</span>.
-            </p>
-            <div className="flex justify-center space-x-4">
+          </h1>
+          <p className="text-xl text-gray-300 mb-8">
+            Discover, share, and review your{" "}
+            <span className="text-green-500">favorite books</span> with a
+            community of <span className="text-green-500">book lovers</span>.
+          </p>
+          <div className="flex justify-center space-x-4">
             <Button
               asChild
               size="lg"
@@ -37,20 +41,22 @@ export default function Home() {
             >
               <Link href="/club-events">Events</Link>
             </Button>
-            </div>
-
+          </div>
         </div>
       </div>
-      <footer className="bg-gray-800 text-white py-4 text-center">
+
+      <footer className="bg-gray-800 text-white py-4 text-center relative z-10">
+        <p className="text-sm">&copy; {new Date().getFullYear()} Reading Circle</p>
         <p className="text-sm">
-          &copy; {new Date().getFullYear()} Reading Circle
-        </p>
-        <p className="text-sm">
-            Developed By
-          {" "}
-            <Link href="https://jumaportfolio.netlify.app/" target="_blank" rel="noopener noreferrer" className="hover:underline text-green-600 dark:text-green-500">
+          Developed By{" "}
+          <Link
+            href="https://jumaportfolio.netlify.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="hover:underline text-green-600 dark:text-green-500"
+          >
             Fred Juma
-            </Link>
+          </Link>
         </p>
       </footer>
     </div>
